74f839fc0e38870da6463f08302c5bbedea05a3b
Lisery <1143765064@qq.com>
2025-10-13 20:34:23 +0800

    feat: 封装win控制按钮组件

---
+++ Orchidic/Controls/WinCtrlButton.axaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+
+namespace Orchidic.Controls;
+
+public partial class WinCtrlButton : UserControl
+{
+    public WinCtrlButton()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public static readonly StyledProperty<double> SizeProperty =
+        AvaloniaProperty.Register<WinCtrlButton, double>(nameof(Size), 24);
+
+    public double Size
+    {
+        get => GetValue(SizeProperty);
+        set => SetValue(SizeProperty, value);
+    }
+
+    public static readonly StyledProperty<IBrush?> FillColorProperty =
+        AvaloniaProperty.Register<WinCtrlButton, IBrush?>(nameof(FillColor), Brushes.Gray);
+
+    public IBrush? FillColor
+    {
+        get => GetValue(FillColorProperty);
+        set => SetValue(FillColorProperty, value);
+    }
+
+    public event EventHandler<RoutedEventArgs>? Click;
+
+    private void InnerButton_Click(object? sender, RoutedEventArgs e)
+    {
+        Click?.Invoke(this, e);
+    }
+
+
+    // 悬浮颜色
+    public static readonly StyledProperty<IBrush?> HoverColorProperty =
+        AvaloniaProperty.Register<WinCtrlButton, IBrush?>(nameof(HoverColor), Brushes.LightGray);
+
+    public IBrush? HoverColor
+    {
+        get => GetValue(HoverColorProperty);
+        set => SetValue(HoverColorProperty, value);
+    }
+
+    // 按下颜色
+    public static readonly StyledProperty<IBrush?> PressedColorProperty =
+        AvaloniaProperty.Register<WinCtrlButton, IBrush?>(nameof(PressedColor), Brushes.DarkGray);
+
+    public IBrush? PressedColor
+    {
+        get => GetValue(PressedColorProperty);
+        set => SetValue(PressedColorProperty, value);
+    }
+
+    // 缩放比例
+    public static readonly StyledProperty<double> PressedScaleProperty =
+        AvaloniaProperty.Register<WinCtrlButton, double>(nameof(PressedScale), 0.85);
+
+    public double PressedScale
+    {
+        get => GetValue(PressedScaleProperty);
+        set => SetValue(PressedScaleProperty, value);
+    }
+}
---
+++ Orchidic/Utils/Converters/HalfValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Avalonia;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+
+namespace Orchidic.Utils.Converters;
+
+public class HalfValueConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is double d)
+            return new CornerRadius(d / 2);
+        return new CornerRadius(0);
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return BindingOperations.DoNothing;
+    }
+}
