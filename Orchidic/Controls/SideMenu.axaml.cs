68d3df7dd3ecea5b7b528cc5c22480bc6b3eff47
Lisery <1143765064@qq.com>
2025-10-16 23:53:39 +0800

    feat: 修改isSelect属性为附加属性

--- Orchidic/Controls/SideMenu.axaml.cs
+++ Orchidic/Controls/SideMenu.axaml.cs
@@ -1,4 +1,5 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
 
 namespace Orchidic.Controls;
 
@@ -8,4 +9,16 @@ public partial class SideMenu : UserControl
     {
         InitializeComponent();
     }
+
+    public static readonly AttachedProperty<bool> IsItemSelectedProperty =
+        AvaloniaProperty.RegisterAttached<SideMenu, Control, bool>(
+            "IsItemSelected",
+            defaultValue: false);
+
+    // ✅ 标准的 getter / setter
+    public static void SetIsItemSelected(AvaloniaObject element, bool value) =>
+        element.SetValue(IsItemSelectedProperty, value);
+
+    public static bool GetIsItemSelected(AvaloniaObject element) =>
+        element.GetValue(IsItemSelectedProperty);
 }
