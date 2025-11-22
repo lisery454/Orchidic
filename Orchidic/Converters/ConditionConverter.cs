68a79977a84f2d0e028a90c458b841aa8a20c261
Lisery <1143765064@qq.com>
2025-11-22 16:28:12 +0800

    feat: 修改播放图标

---
+++ Orchidic/Converters/ConditionConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace Orchidic.Converters;
+
+public class ConditionConverter : IMultiValueConverter
+{
+    public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+    {
+        var condition = values[0] as bool? ?? false;
+        var obj1 = values[1];
+        var obj2 = values[2];
+
+        return condition ? obj1 : obj2;
+    }
+
+    public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+    {
+        return [];
+    }
+}
---
+++ Orchidic/Views/Components/SvgIconButton.xaml.cs
@@ -0,0 +1,64 @@
+﻿namespace Orchidic.Views.Components;
+
+public partial class SvgIconButton
+{
+    public SvgIconButton()
+    {
+        InitializeComponent();
+    }
+
+    // ▶ 图标 Geometry
+    public Geometry IconData
+    {
+        get => (Geometry)GetValue(IconDataProperty);
+        set => SetValue(IconDataProperty, value);
+    }
+    public static readonly DependencyProperty IconDataProperty =
+        DependencyProperty.Register(nameof(IconData), typeof(Geometry), typeof(SvgIconButton));
+
+    // ▶ 默认状态颜色
+    public Brush IconBrush
+    {
+        get => (Brush)GetValue(IconBrushProperty);
+        set => SetValue(IconBrushProperty, value);
+    }
+    public static readonly DependencyProperty IconBrushProperty =
+        DependencyProperty.Register(nameof(IconBrush), typeof(Brush), typeof(SvgIconButton));
+
+    // ▶ 悬浮颜色
+    public Brush HoverBrush
+    {
+        get => (Brush)GetValue(HoverBrushProperty);
+        set => SetValue(HoverBrushProperty, value);
+    }
+    public static readonly DependencyProperty HoverBrushProperty =
+        DependencyProperty.Register(nameof(HoverBrush), typeof(Brush), typeof(SvgIconButton));
+
+    // ▶ 按下颜色
+    public Brush PressedBrush
+    {
+        get => (Brush)GetValue(PressedBrushProperty);
+        set => SetValue(PressedBrushProperty, value);
+    }
+    public static readonly DependencyProperty PressedBrushProperty =
+        DependencyProperty.Register(nameof(PressedBrush), typeof(Brush), typeof(SvgIconButton));
+
+
+    // ▶ Command
+    public ICommand Command
+    {
+        get => (ICommand)GetValue(CommandProperty);
+        set => SetValue(CommandProperty, value);
+    }
+    public static readonly DependencyProperty CommandProperty =
+        DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(SvgIconButton));
+
+    // ▶ CommandParameter
+    public object CommandParameter
+    {
+        get => GetValue(CommandParameterProperty);
+        set => SetValue(CommandParameterProperty, value);
+    }
+    public static readonly DependencyProperty CommandParameterProperty =
+        DependencyProperty.Register(nameof(CommandParameter), typeof(object), typeof(SvgIconButton));
+}
